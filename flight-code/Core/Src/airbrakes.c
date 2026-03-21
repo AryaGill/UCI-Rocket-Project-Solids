56855d7f6a8e6112d62d3e2fd5617e55cf8bb2b2
BradyCason <bradyrcason@gmail.com>
2026-03-21 02:59:37 -0700

    Updated percentage off of airbrakes deployment

--- flight-code/Core/Src/airbrakes.c
+++ flight-code/Core/Src/airbrakes.c
@@ -241,8 +241,8 @@ void set_airbrakes_deployment_level(Telemetry_t *telemetry, uint8_t deployment){
 
 	telemetry->airbrake_deployment = deployment;
 
-	// if deployment > 0.668 (max deployment of current night fury), then set to max
-	float real_max_percent_of_theoretical_max = 0.668;
+	// if deployment > 851 (max deployment of current night fury), then set to max
+	float real_max_percent_of_theoretical_max = 0.851;
 	float t = (float)deployment / ((float)(NUM_DEPLOYMENT_LEVELS - 1) * real_max_percent_of_theoretical_max);
 	t = clampf(t, 0, 1);
 	float angle = SERVO_ANGLE_NOT_EXTENDED + t * (SERVO_ANGLE_EXTENDED - SERVO_ANGLE_NOT_EXTENDED);
